f2baa0a479f7daa53f3ce7b125e467888c4ca3f9
Yuri Belleri <yuribelleri@gmail.com>
2024-04-14 22:18:07 +0200

    Adding telescope

--- lua/zioyuri78/packer.lua
+++ lua/zioyuri78/packer.lua
@@ -30,6 +30,12 @@ return require('packer').startup(function(use)
 		run = ':TSUpdate'
 	}
 
+	use {
+  		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+  		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
 	use 'ellisonleao/gruvbox.nvim'
 	use 'williamboman/mason.nvim'
 	use 'williamboman/mason-lspconfig.nvim'
