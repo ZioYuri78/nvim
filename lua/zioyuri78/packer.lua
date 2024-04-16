1152a0465df0e519e41a86aeffabd083b1999860
Yuri Belleri <yuribelleri@gmail.com>
2024-04-16 13:18:57 +0200

    Updated Lua Snippets

--- lua/zioyuri78/packer.lua
+++ lua/zioyuri78/packer.lua
@@ -42,8 +42,10 @@ return require('packer').startup(function(use)
 	use 'neovim/nvim-lspconfig'
 	use 'hrsh7th/nvim-cmp'
 	use 'hrsh7th/cmp-nvim-lsp'
-	use 'L3MON4D3/LuaSnip'
-
+    use { 
+		"L3MON4D3/LuaSnip", 
+		run = "make install_jsregexp" 
+	}
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
