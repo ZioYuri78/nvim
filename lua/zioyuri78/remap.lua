e277c9badec68b5bbad5cab6edbdd7a6c8b4fa90
Yuri Belleri <yuribelleri@gmail.com>
2024-04-23 15:35:55 +0200

    Split and resize hotkeys

--- lua/zioyuri78/remap.lua
+++ lua/zioyuri78/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<C-s>'    , ':split<CR>')
+vim.keymap.set('n', '<M-Right>', ':vertical resize +1<CR>')
+vim.keymap.set('n', '<M-Left>' , ':vertical resize -1<CR>')
+vim.keymap.set('n', '<M-Up>'   , ':resize +1<CR>')
+vim.keymap.set('n', '<M-Down>' , ':resize -1<CR>')
