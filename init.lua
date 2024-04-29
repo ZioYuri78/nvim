f2a88f7dc2685c366296f3dadaf5c48fc0e517ae
Yuri Belleri <yuribelleri@gmail.com>
2024-04-29 13:58:14 +0200

    OS specific python path

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 vim.g.loaded_python_provider = 0
-vim.g.python3_host_prog = '../Programs/Python/Python312'
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	vim.g.python3_host_prog = '../Programs/Python/Python312'
+else
+	vim.g.python3_host_prog = '/home/zioyuri78/anaconda/bin/python'
+end
 vim.g.pyxversion = 3
 
 require("zioyuri78")
