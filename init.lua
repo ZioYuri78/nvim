0d9e961f8e4d486fd590abf2e5072d83826172ed
Yuri Belleri <yuribelleri@gmail.com>
2024-04-29 16:01:55 +0200

    Maybe finally it's now ok

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.g.loaded_python_provider = 0
 if vim.loop.os_uname().sysname == "Windows_NT" then
-	vim.g.python3_host_prog = '../Programs/Python/Python312'
+	vim.g.python3_host_prog = "C:/Program Files/Python312/python.exe"
 else
 	vim.g.python3_host_prog = '/home/zioyuri78/anaconda3/bin/python'
 end
