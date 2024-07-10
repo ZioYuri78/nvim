7edac4beafb4d5b55a1afc71295db56a5194c475
Yuri Belleri <yuribelleri@gmail.com>
2024-07-10 16:29:13 +0200

    git stage/unstage from nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,3 +1,28 @@
+-- Git stage/unstage files and directories from the tree
+local api = require("nvim-tree.api")
+
+local git_add = function()
+  local node = api.tree.get_node_under_cursor()
+  local gs = node.git_status.file
+
+  -- If the current node is a directory get children status
+  if gs == nil then
+    gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
+	or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+  end
+
+  -- If the file is untracked, unstaged or partially staged, we stage it
+  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    vim.cmd("silent !git add " .. node.absolute_path)
+
+  -- If the file is staged, we unstage
+  elseif gs == "M " or gs == "A " then
+    vim.cmd("silent !git restore --staged " .. node.absolute_path)
+  end
+
+  api.tree.reload()
+end
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -6,3 +31,4 @@ require("nvim-tree").setup()
 
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>', {})
 vim.keymap.set('n', '<C-f>', ':NvimTreeFocus<CR>', {})
+vim.keymap.set('n', '<leader>ga', git_add, opts('Git Add'))
