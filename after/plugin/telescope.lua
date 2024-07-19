ffb6ad155a0611469173054e8326cfa91ec18e14
Yuri Belleri <yuribelleri@gmail.com>
2024-07-19 16:19:21 +0200

    Open telescope keybind <leader>tt

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>tt', ":Telescope<CR>", {})
 vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
